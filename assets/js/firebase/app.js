218e814f52551c409209726f8c314a1ce570cb65
Reginaldo Tobias <reginaldotobias.14@gmail.com>
2022-08-26 20:46:45 -0300

    Receber dados no Firebase

--- assets/js/firebase/app.js
+++ assets/js/firebase/app.js
@@ -1,11 +1,10 @@
-  // Import the functions you need from the SDKs you need
+
+ // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyBceCHSEsRdz9iAkmkvUq4B-Nv0J7E-6Sc",
     authDomain: "mundo-inverto.firebaseapp.com",
@@ -18,5 +17,3 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>

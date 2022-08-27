906cb9fca97a4c949171727b3d1a3588e2300615
Reginaldo Tobias <reginaldotobias.14@gmail.com>
2022-08-26 21:28:01 -0300

    Integrando os dados com o Firebase

--- assets/js/firebase/app.js
+++ assets/js/firebase/app.js
@@ -17,3 +17,5 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  
+  export default app
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 
+import { subscribeToHellfireClub } from './firebase/hellfire-clube.js'
+
 const txtName = document.getElementById("txtName")
 const txtEmail = document.getElementById("txtEmail")
 const txtLevel = document.getElementById("txtLevel")
@@ -6,13 +8,14 @@ const txtCharacter = document.getElementById("txtCharacter")
 
 const txtbtnSubscribe = document.getElementById("txtbtnSubscribe")
 
-btnSubscribe.addEventListener("click", () => {
+btnSubscribe.addEventListener("click", async () => {
     const subscription = {
         nome: txtName.value,
         email: txtEmail.value,
         level: txtLevel.value,
         character: txtCharacter.value,
     }
-    // Salvar no banco de dados:
-    console.log(subscription)
+    
+    const subscriptionID = await subscribeToHellfireClub(subscription)
+    console.log('Iscrito com sucesso: ${subscripionId}') 
 })
